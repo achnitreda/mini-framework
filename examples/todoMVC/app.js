081558bf45056144475901c158c989b462db128a
achnitreda <achnitreda@gmail.com>
2025-03-29 13:10:20 +0000

    implementing a basic DOM abstraction using Virtual-DOM

--- examples/todoMVC/app.js
+++ examples/todoMVC/app.js
@@ -1,1 +1,36 @@
-console.log("App is loading")
+const button = MiniFramework.createElement('button', { 
+    class: 'btn',
+    onClick: (e) => {
+      console.log('Button clicked!', e);
+    }
+  }, 'Click me');
+
+const vNode = MiniFramework.createElement('div', { class: 'container' },
+    MiniFramework.createElement('h1', { class: 'title' }, 'Hello MiniFramework'),
+    MiniFramework.createElement('p', { class: 'content' }, 'This is a paragraph'),
+    MiniFramework.createElement('button', {
+        class: 'btn primary',
+        id: 'submit-btn',
+        disabled: true,
+        style: 'background-color: blue; color: white;'
+    }, 'Click me'),
+    MiniFramework.createElement('div', { class: 'card' },
+        MiniFramework.createElement('div', { class: 'card-header' },
+            MiniFramework.createElement('h2', {}, 'Card Title')
+        ),
+        MiniFramework.createElement('div', { class: 'card-body' },
+            MiniFramework.createElement('p', {}, 'Card content goes here'),
+            MiniFramework.createElement('button', {
+                class: 'btn',
+                onClick: () => alert('Button clicked')
+            }, 'Click me')
+        )
+    ),
+    button,
+);
+
+// Render to DOM
+const appContainer = document.getElementById('app')
+MiniFramework.render(vNode, appContainer);
+
+
--- mini-framework.js
+++ mini-framework.js
@@ -46,14 +46,57 @@ const MiniFramework = {
         return element
     },
 
+
     /**
-     * Renders a virtual DOM tree to a container
+     * Store the current Virtual DOM tree for each container
+     * @type {Map<HTMLElement,Object>}
+     */
+    _vDomTrees: new Map(),
+
+
+    /**
+     * Deep clones  a virtual DOM node to prevent reference issues
+     * @param {Object|string|number|boolean|null|undefined} node - Node to clone
+     * @returns {Object|string|number|boolean|null|undefined} Cloned node
+     */
+    _deepCloneVNode(node) {
+        if (node == null || typeof node !== 'object') {
+            return node;
+        }
+
+        // Clone the node
+        const clone = { tag: node.tag };
+
+        if (node.attrs) {
+            clone.attrs = {};
+            for (const [key, value] of Object.entries(node.attrs)) {
+                clone.attrs[key] = value;
+            }
+        }
+
+        if (node.children) {
+            clone.children = node.children.map(child => this._deepCloneVNode(child))
+        }
+
+        return clone
+    },
+
+    /**
+     * Renders a virtual DOM tree to a container using efficient diffing
      * @param {Object} vNode - Virtual DOM node
      * @param {HTMLElement} container - Container to render into
      */
     render(vNode, container) {
-        container.innerHTML = ''
-        container.appendChild(this.createRealElement(vNode))
+        const oldVNode = this._vDomTrees.get(container)
+
+        if (!oldVNode) {
+            container.innerHTML = ''
+            container.appendChild(this.createRealElement(vNode))
+        } else {
+            this.updateElement(container, vNode, oldVNode, 0)
+        }
+
+        this._vDomTrees.set(container, this._deepCloneVNode(vNode))
     },
 
     /**
@@ -63,7 +106,89 @@ const MiniFramework = {
      * @returns {boolean} True if nodes are different
      */
     changed(node1, node2) {
-        console.log("loading")
-        return false
-    }
+        if (node1 == null || node2 == null) return node1 != node2
+
+        if (typeof node1 !== typeof node2) return true
+
+        if (typeof node1 !== 'object') return node1 !== node2
+
+        return node1.tag !== node2.tag
+    },
+
+    /**
+     * Updates DOM elements based on Virtual DOM differences (Diffing & Patching)
+     * @param {HTMLElement} parent - Parent element
+     * @param {Object|string|number|boolean|null|undefined} newNode - New Virtual Node
+     * @param {Object|string|number|boolean|null|undefined} oldNode - Old Virtual Node
+     * @param {number} index - Index of child in parent
+     */
+    updateElement(parent, newNode, oldNode, index = 0) {
+        if (oldNode == null) {
+            parent.appendChild(this.createRealElement(newNode))
+            return
+        }
+
+        if (newNode == null) {
+            parent.removeChild(parent.childNodes[index])
+            return
+        }
+
+        if (this.changed(newNode, oldNode)) {
+            parent.replaceChild(this.createRealElement(newNode), parent.childNodes[index])
+            return
+        }
+
+        if (typeof newNode !== 'object' || typeof oldNode !== 'object') {
+            return;
+        }
+
+        // At this point we know both nodes are objects with the same tag
+
+        /** 
+         * @type {HTMLElement} 
+         * => I skip this because I know for sure that I have Node element
+         */
+        // @ts-ignore
+        const el = parent.childNodes[index]
+
+        const newAttrs = newNode.attrs || {}
+        const oldAttrs = oldNode.attrs || {}
+
+        for (const [attr, value] of Object.entries(newAttrs)) {
+            if (attr.startsWith('on') && !(attr in oldAttrs)) {
+                const eventName = attr.slice(2).toLowerCase()
+                el.addEventListener(eventName, value)
+            } else if (oldAttrs[attr] !== value) {
+                el.setAttribute(attr, value)
+            }
+        }
+
+        for (const attr in oldAttrs) {
+            if (attr.startsWith('on')) {
+                const eventName = attr.slice(2).toLowerCase()
+                // Remove old handler if it doesn't exist in new attributes or is different
+                if (!(attr in newAttrs)) {
+                    el.removeEventListener(eventName, oldAttrs[attr])
+                } else if (oldAttrs[attr] !== newAttrs[attr]) {
+                    el.removeEventListener(eventName, oldAttrs[attr])
+                    el.addEventListener(eventName, newAttrs[attr])
+                }
+            } else if (!(attr in newAttrs)) {
+                el.removeAttribute(attr)
+            }
+        }
+
+        const newChildren = newNode.children || []
+        const oldChildren = oldNode.children || []
+        const maxLength = Math.max(newChildren.length, oldChildren.length)
+
+        for (let i = 0; i < maxLength; i++) {
+            this.updateElement(
+                el,
+                i < newChildren.length ? newChildren[i] : null,
+                i < oldChildren.length ? oldChildren[i] : null,
+                i
+            )
+        }
+    },
 }
